e6c0f26952770d5ab4ee7b4b9614efcfbc6d7899
aliffrhn <alif.farhanw@gmail.com>
2019-06-24 12:11:28 +0700

    create MenuUtama

---
+++ Components/MenuUtama.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+export default class MenuUtama extends Component{
+
+    render(){
+
+        return(
+            <View>
+                <Text>
+                    Menu 1
+                </Text>
+                <Text>
+                    Menu 2
+                </Text>
+            </View>
+        )
+    }
+}
