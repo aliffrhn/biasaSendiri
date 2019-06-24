7f712f60e4ac835bf2ce408d6222f2411f388675
aliffrhn <alif.farhanw@gmail.com>
2019-06-24 13:59:10 +0700

    add header and footer

--- App.js
+++ App.js
@@ -9,31 +9,35 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
+
+Header = () => {
+
+  return(
+    <View>
+      <Text>
+        Header
+      </Text>
+    </View>
+  )
+}
+
+Footer = () => {
+
+    return(
+      <View>
+        <Text>
+          Footer
+        </Text>
+      </View>
+    )
+}
+
 export default class App extends Component<Props> {
   render() {
     return (
       <View>
-
+        <Header/>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
